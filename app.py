c65682595f8c422ce943a9f74d2f0b833255fff2
Vansh2693 <vanshraj2693@gmail.com>
2025-08-01 23:16:57 +0530

    Added Model Web Interface

---
+++ app.py
@@ -0,0 +1,48 @@
+import joblib
+import numpy as np
+from config.paths_config import MODEL_OUTPUT_PATH
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+loaded_model = joblib.load(MODEL_OUTPUT_PATH)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods = ['GET','POST'])
+def predict():
+    if request.method=="POST":
+        lead_time = int(request.form["lead_time"])
+        no_of_special_request = int(request.form["no_of_special_request"])
+        avg_price_per_room = float(request.form["avg_price_per_room"])
+        arrival_month = int(request.form["arrival_month"])
+        arrival_date = int(request.form["arrival_date"])
+        market_segment_type = int(request.form["market_segment_type"])
+        no_of_week_nights = int(request.form["no_of_week_nights"])
+        no_of_weekend_nights = int(request.form["no_of_weekend_nights"])
+        type_of_meal_plan = int(request.form["type_of_meal_plan"])
+        room_type_reserved = int(request.form["room_type_reserved"])
+
+        features = np.array([[lead_time, no_of_special_request, avg_price_per_room, arrival_month, arrival_date, market_segment_type, no_of_week_nights, no_of_weekend_nights, type_of_meal_plan, room_type_reserved]])
+        
+        prediction = loaded_model.predict(features)
+        
+        return render_template('predict.html',prediction=prediction[0])
+    
+    return render_template("predict.html",prediction=None)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
